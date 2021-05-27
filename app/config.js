c4d597faccbbce4e451019c850a67d4f0e6e57aa
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-05-27 12:08:08 +0100

    Victorias changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Care Hub",
+  serviceName: "Care Workforce Hub",
 
   // Default port that prototype runs on
   port: "3000",
